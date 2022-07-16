d66422a7c5c12eeb246d2df5357d566e30727960
XtreeMaJI <xtreemaji@gmail.com>
2022-07-16 20:50:51 +0300

    Сюрикены застревают в объектах слоя "branch"

---
+++ Assets/Scripts/Shuriken.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Shuriken : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Branch")
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.detectCollisions = false;
+        }
+    }
+
+}
