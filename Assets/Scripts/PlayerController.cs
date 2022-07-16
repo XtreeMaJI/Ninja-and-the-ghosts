9396f9e45d7f641a3092d77a7f6bc34edec3d63c
XtreeMaJI <xtreemaji@gmail.com>
2022-07-16 14:23:28 +0300

    Добавил игроку возможность кидаться сюрикенами в направлении клика мышью

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ public class PlayerController : MonoBehaviour
     private const float MAX_RAY_DISTANCE = 100f;
 
     public Camera mainCamera;
-    public GameObject spawnObj;
+    public GameObject debugHitObj;
+
+    public GameObject shurikenInstance;
+    public float shurikenSpeed = 5f;
 
     void Start()
     {
@@ -14,31 +17,39 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
-        Vector3 screenMousePos = Input.mousePosition;
-        Ray rayFromMouse = mainCamera.ScreenPointToRay(screenMousePos);
-
-        Debug.DrawRay(mainCamera.transform.position, rayFromMouse.direction * 100, Color.yellow);
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            ThrowRope();
+            ThrowShuriken();
         }
+        
     }
 
-    private void ThrowRope()
+    private void ThrowShuriken()
     {
-        Vector3 screenMousePos = Input.mousePosition;
-        Ray rayFromMouse = mainCamera.ScreenPointToRay(screenMousePos);
+        Vector3 throwDir = GetMouseClickPoint() - transform.position;
+        throwDir.Normalize();
 
-        Debug.DrawRay(mainCamera.transform.position, rayFromMouse.direction *100, Color.yellow);
+        GameObject shuriken = Instantiate(shurikenInstance, transform.position, Quaternion.identity);
+        Rigidbody shurikenRb = shuriken.GetComponent<Rigidbody>();
+        if (shurikenRb)
+            shurikenRb.velocity = throwDir * shurikenSpeed;
+
+    }
+
+    //Получаем точку на плоскости MouseRayBlocker, куда кликнули мышкой
+    private Vector3 GetMouseClickPoint()
+    {
+        Vector3 clickPoint = new Vector3(0, 0, 0);
+
+        Vector3 screenMousePos = Input.mousePosition;
+        Ray rayToMousePos = mainCamera.ScreenPointToRay(screenMousePos);
 
         RaycastHit rayInfo;
         int layerMask = 1 << LayerMask.NameToLayer("MouseRayBlocker");
-        if (Physics.Raycast(rayFromMouse, out rayInfo, MAX_RAY_DISTANCE, layerMask))
-        {
-            Instantiate(spawnObj, rayInfo.point, Quaternion.identity);
-        }
-
+        if (Physics.Raycast(rayToMousePos, out rayInfo, MAX_RAY_DISTANCE, layerMask))
+            clickPoint = rayInfo.point;
+        
+        return clickPoint;
     }
 
 }
