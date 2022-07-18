1d5460ca28d665f11d3f123d7a20a041d26363fe
Polina Amelina <anathebeequeen@gmail.com>
2022-07-19 00:08:06 +0300

    Сделал генерацию уровня

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public const float MIN_DISTANCE_FOR_GENERATE = 25f;
+
+    public GameObject branchInstance;
+    
+    public GameObject playerObj;
+
+    private GameObject lastBranch;
+
+    private void Start()
+    {
+        lastBranch = FindObjectOfType<Branch>().gameObject;
+    }
+
+    void Update()
+    {
+        if (ShouldGenerateBranch())
+            GenerateBranch();
+    }
+
+
+    void GenerateBranch()
+    {
+        if (!branchInstance || !lastBranch)
+            return;
+
+        float branchesInterval = 25f;
+        Vector3 lastBranchPos = lastBranch.transform.position;
+        Vector3 branchPos = new Vector3(lastBranchPos.x + branchesInterval, lastBranchPos.y, lastBranchPos.z);
+        lastBranch = Instantiate(branchInstance, branchPos, Quaternion.identity);
+    }
+
+    bool ShouldGenerateBranch() 
+    {
+        float distance = Mathf.Abs(playerObj.transform.position.x - lastBranch.transform.position.x);
+        if (distance < MIN_DISTANCE_FOR_GENERATE)
+            return true;
+
+        return false;
+    }
+
+}
