49fef6f81ba78bd9912e1fc3d435c8ecb3e17319
Polina Amelina <anathebeequeen@gmail.com>
2022-07-20 22:07:19 +0300

    Добавил проигрыш, если игрок упал слишком низко и перенёс скрипты, связанные с движением игрока, в отдельную папку

---
+++ Assets/Scripts/DeathZoneZ.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZoneZ : MonoBehaviour
+{
+    private LevelManager lm;
+
+    private void Start()
+    {
+        lm = Object.FindObjectOfType<LevelManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+            lm.GameOver();
+    }
+
+}
