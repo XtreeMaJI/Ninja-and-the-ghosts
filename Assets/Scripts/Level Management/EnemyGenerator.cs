eb9a2d2d1ea32c4f2616060884acec01969d379e
Polina Amelina <anathebeequeen@gmail.com>
2022-07-21 21:54:15 +0300

    Доделал генератор врагов

--- Assets/Scripts/Level Management/EnemyGenerator.cs
+++ Assets/Scripts/Level Management/EnemyGenerator.cs
@@ -6,8 +6,11 @@ public class EnemyGenerator : MonoBehaviour
 {
     //Каждый раз, когда счёт увеличится на это значение будет проверка на то, нужно ли спавнить врага
     public int scoreBetweenSpawn = 10;
-    public float spawnDistanceFromPlayer = 25; //Расстояние от игрока, на котором появится враг
-    public float spawnShiftY = 0; //Смещение от минимального уровня для спавна врага
+    public float spawnDistanceFromPlayer = 50; //Расстояние от игрока, на котором появится враг
+
+    //Смещения по высоте для спавна врага
+    public float bottomSpawnShiftY = 0; 
+    public float topSpawnShiftY = 1;
 
     public GameObject enemyInstance;
 
@@ -15,6 +18,10 @@ public class EnemyGenerator : MonoBehaviour
 
     private LevelManager lm;
     private LevelGenerator levelGenerator;
+
+    private int lastCheckedStore = 0; //Список счетов, на которых уже проводилась проверка на необходимость спавна
+
+    public int spawnProbThresh = 10; //Количесвтов проверок на спавн, после которого враг будет спавниться после каждой проверки
 
     private void Start()
     {
@@ -24,19 +31,28 @@ public class EnemyGenerator : MonoBehaviour
 
     private void Update()
     {
-        if (lm.score != 0 && lm.score % scoreBetweenSpawn == 0 && isShouldSpawn())
-            SpawnEnemy();
+        if (lm.score != 0 && lm.score % scoreBetweenSpawn == 0 && lm.score > lastCheckedStore)
+        {
+            lastCheckedStore = lm.score;
+            if (isShouldSpawn())
+                SpawnEnemy();
+        }
+            
     }
 
     private bool isShouldSpawn()
     {
-        return true;
+        //Чем больше проверок на спавн врага прошли, тем больше вероятность спавна
+        int checksCount = lastCheckedStore / scoreBetweenSpawn;
+        float maxProb = (float)checksCount / (float)spawnProbThresh;
+
+        return Random.Range(0f, 1f) <= maxProb;
     }
 
     private void SpawnEnemy()
     {
-        float x = lm.transform.position.x + spawnDistanceFromPlayer;
-        float y = Random.Range(deathZoneZTransform.position.y + spawnShiftY, levelGenerator.branchYPos);
+        float x = lm.playerObj.transform.position.x + spawnDistanceFromPlayer;
+        float y = Random.Range(deathZoneZTransform.position.y + bottomSpawnShiftY, levelGenerator.branchYPos - topSpawnShiftY);
         Vector3 spawnPos = new Vector3(x, y, 0f);
 
 
